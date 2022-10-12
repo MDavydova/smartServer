80889d7fb2d39760f183e1d4505cb9eaecbe8a55
Milena Davydova <milena@yelk.com.ua>
2022-10-12 13:08:01 +0200

    refactoring

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,12 +1,10 @@
-const handleSignin = (req, res, db, bcrypt) => {
-    const {email, password} = req.body;
-
-    if(!email || !password) {
-        return res.status(400).json('incorrect from submission')
+const handleSignin = (db, bcrypt) => (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(400).json('incorrect form submission');
     }
-
     db.select('email', 'hash').from('login')
-        .where('email', '=',email)
+        .where('email', '=', email)
         .then(data => {
             const isValid = bcrypt.compareSync(password, data[0].hash);
             if (isValid) {
@@ -22,7 +20,6 @@ const handleSignin = (req, res, db, bcrypt) => {
         })
         .catch(err => res.status(400).json('wrong credentials'))
 }
-
 
 module.exports = {
     handleSignin: handleSignin
