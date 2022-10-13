4a055530bb4116e40795cd0d3b799f3779e39912
Milena Davydova <milena@yelk.com.ua>
2022-10-13 10:07:16 +0200

    adding port

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcry
 app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT,()=> {
     console.log(`app is running on port ${PORT}`);
